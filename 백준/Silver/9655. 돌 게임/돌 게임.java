f69c009806d7f63a9ad3b9daeab477e16c27e3db
hen715 <130031561+hen715@users.noreply.github.com>
2024-01-13 07:33:16 +0900

    [Silver V] Title: 돌 게임, Time: 204 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+       int a = scanner.nextInt();
+
+       if(a%2==1)
+           System.out.println("SK");
+       else
+           System.out.println("CY");
+
+    }
+
+}
