7493183680c6982f6fcb97760887cdfd574ce22e
hen715 <130031561+hen715@users.noreply.github.com>
2024-08-09 16:34:02 +0900

    [Bronze V] Title: Rook, Time: 96 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+
+
+class Main {
+
+
+    public static void main(String[] args) throws IOException{
+        System.out.println("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+    }
+}
