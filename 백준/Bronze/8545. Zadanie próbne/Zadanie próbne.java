dd7424c3f5fc251020f2296575fc119f83e49bbd
hen715 <130031561+hen715@users.noreply.github.com>
2024-08-24 20:14:38 +0900

    [Bronze V] Title: Zadanie próbne, Time: 96 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        sb.reverse();
+        System.out.println(sb);
+    }
+
+}
