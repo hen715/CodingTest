66a33acf46f5761ee4b88cfc8381fe2f081da7cc
hen715 <130031561+hen715@users.noreply.github.com>
2024-09-01 16:47:13 +0900

    [Bronze V] Title: 웰컴, Time: 92 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,9 @@
+class Main {
+
+    public static void main(String[] args){
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+
+    }
+}
