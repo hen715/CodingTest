86870cf538b94ddcbd30dcda18513e77972d1c7d
hen715 <130031561+hen715@users.noreply.github.com>
2024-09-02 17:22:14 +0900

    [Silver I] Title: IOIOI, Time: 288 ms, Memory: 26988 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -2,42 +2,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
-
 class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-        String p = "I";
-        int count = 0;
-        for(int i = 0 ; i < N ; i++){
-            p += "OI";
-        }
+        int[] pi = new int[1 + 2 * N];
+        String S = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        sb.append("I");
+        sb.append("OI".repeat(N));
+        String pattern = sb.toString();
+
         int index = 0;
-        while (s.length()-index>=p.length()){
-            String temp = "";
-            if(s.charAt(index)=='I'){
-                for(int i = 0 ; i < p.length(); i++){
-                    int now = i + index;
-                    temp += s.charAt(now);
-                    if(temp.charAt(i)!=p.charAt(i)){
-                        break;
-                    }
-                }
-                if(temp.equals(p)){
+
+        for(int i = 1 ;i < 1 + 2 *N ; i++ ){
+
+            while(index > 0  && pattern.charAt(i) != pattern.charAt(index)){
+                index = pi[index -1];
+            }
+            if(pattern.charAt(i)==pattern.charAt(index)){
+                index++;
+                pi[i] = index;
+            }
+        }
+
+        index = 0;
+        int count = 0;
+        for(int i = 0 ; i < M ; i++){
+            while (index > 0 && S.charAt(i)!=pattern.charAt(index)){
+                index = pi[index - 1];
+            }
+            if(S.charAt(i)==pattern.charAt(index)){
+                if(index == pattern.length() - 1){
                     count++;
-                    index += 2;
-                    continue;
+                    index = pi[index];
+                }else{
+                    index++;
                 }
             }
-            index++;
         }
         System.out.println(count);
-
-
 
     }
 }
