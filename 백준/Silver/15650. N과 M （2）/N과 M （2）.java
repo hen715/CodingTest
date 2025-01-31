c64689e0b9bb40f52b02141ba9387ade6027774d
hen715 <130031561+hen715@users.noreply.github.com>
2025-01-31 13:37:20 +0900

    [Silver III] Title: N과 M (2), Time: 132 ms, Memory: 16160 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+class Main {
+
+    static StringBuilder sb;
+    static int N;
+    static int M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+        backTracking(1,0,"");
+        System.out.println(sb);
+    }
+
+    public static void backTracking(int start, int length, String str){
+        if(length == M){
+            sb.append(str).append("\n");
+            return;
+        }
+        for(int i = start ; i <= N ; i++){
+            if(length>0&&i == start) continue;
+            String s = str + i +" ";
+            backTracking(i,length+1,s);
+        }
+
+    }
+}
