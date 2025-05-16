216619de03af629d6ed90b8860aa48069dd3f397
hen715 <130031561+hen715@users.noreply.github.com>
2025-05-16 16:37:48 +0900

    [Gold IV] Title: 팰린드롬?, Time: 924 ms, Memory: 306248 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -12,29 +12,35 @@ class Main {
         for(int i = 0 ; i < N ; i++){
             list[i] = Integer.parseInt(s[i]);
         }
+        int[][] dp = new int[N][N];
+        for(int i = 0 ; i < N ; i++){
+            dp[i][i] = 1;
+        }
+        for(int i = 1 ; i < N  ; i++){
+            int start = 0;
+            int end = i;
+            while (end < N){
+                if (list[start] != list[end]) {
+                    dp[start][end] = 0;
+                }
+                else{
+                    if(end - start ==1){
+                        dp[start][end] = 1;
+                    }
+                    else if(dp[start+1][end-1]==1){
+                        dp[start][end] = 1;
+                    }
+                }
+                start++;
+                end++;
+            }
+        }
         int M = Integer.parseInt(br.readLine());
         for(int i = 0 ; i < M ; i++){
             s = br.readLine().split(" ");
             int S = Integer.parseInt(s[0]) - 1;
             int E = Integer.parseInt(s[1]) - 1;
-            boolean flag = true;
-            while (S<E){
-                if(list[S]==list[E]){
-                    S++;
-                    E--;
-                }
-                else{
-                    flag = false;
-                    break;
-                }
-
-            }
-            if(flag){
-                sb.append(1).append("\n");
-            }
-            else{
-                sb.append(0).append("\n");
-            }
+            sb.append(dp[S][E]).append('\n');
         }
         System.out.print(sb);
     }
