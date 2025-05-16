9ff94c0076d354a5fb6153ea85064c54c4599237
hen715 <130031561+hen715@users.noreply.github.com>
2025-05-16 23:29:57 +0900

    [Gold IV] Title: 사이클 게임, Time: 1164 ms, Memory: 285860 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+
+class Main {
+    static int N;
+    static int[] list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+        list = new int[N];
+        for(int i = 0 ; i < N ; i++){
+            list[i] = i;
+        }
+        int point = 0;
+        for(int i = 0 ; i < M ; i++){
+            s = br.readLine().split(" ");
+            int start = Math.min(Integer.parseInt(s[0]),Integer.parseInt(s[1]));
+            int end = Math.max(Integer.parseInt(s[0]),Integer.parseInt(s[1]));
+            if(point!=0){
+                continue;
+            }
+            if(check(start,end)){
+                point = i+1;
+            }
+            else {
+                int a = parent(start);
+                int b = parent(end);
+                list[a] = b;
+            }
+
+        }
+        System.out.print(point);
+    }
+    public static boolean check(int a, int b){
+        return parent(a)==parent(b);
+    }
+
+    public static int parent(int num){
+        if(list[num]==num){
+            return num;
+        }
+        return list[num] = parent(list[num]);
+    }
+}
