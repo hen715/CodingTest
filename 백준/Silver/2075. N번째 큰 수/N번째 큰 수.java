7f458458f8106e720d76a5544fbbeb85367b12a7
hen715 <130031561+hen715@users.noreply.github.com>
2025-07-24 10:53:17 +0900

    [Silver III] Title: N번째 큰 수, Time: 840 ms, Memory: 284760 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,28 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
-class Main {
+public class Main {
+	
+	static void board(int[][] arr) {
+		for (int[] ia : arr) System.out.println(Arrays.toString(ia));
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> o2-o1);
-        int N = Integer.parseInt(br.readLine());
-        for(int i = 0 ; i < N ; i++){
-            String[] s = br.readLine().split(" ");
-            for(int j = 0 ; j < N ; j++){
-                queue.add(Integer.parseInt(s[j]));
-            }
-        }
-        for(int i = 0 ; i < N-1 ; i++){
-            queue.poll();
-        }
-        System.out.println(queue.poll());
-
-
-
-    }
-
-
+	public static void main(String[] args) throws Exception{
+		//System.setIn(new FileInputStream("src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][N];
+		String str;
+		for (int i = 0; i < N; i++) {
+			str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str, " ");
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int[] x: arr) {
+			for (int val : x) {
+				maxHeap.add(val);
+			}
+		}
+		int result = -1;
+		for (int i = 0; i < N; i++) {
+			result = maxHeap.poll();
+		}
+		System.out.println(result);
+	}
 }
