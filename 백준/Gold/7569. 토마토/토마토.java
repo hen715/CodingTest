95efd115818b742d89acd69e3ad4d9ccc23307ee
hen715 <130031561+hen715@users.noreply.github.com>
2025-07-24 15:53:35 +0900

    [Gold V] Title: 토마토, Time: 576 ms, Memory: 104396 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,119 +1,105 @@
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int n;
-    public static int m;
-    public static int h;
+	final static int[] di = { 0, 1, 0, -1, 0, 0 };
+	final static int[] dj = { -1, 0, 1, 0, 0, 0 };
+	final static int[] dk = { 0, 0, 0, 0, -1, 1 };
+	static int M, N, H, unripedT;
+	static ArrayDeque<int[]> q = new ArrayDeque<>();
 
-    public static int[][] list;
-    public static int[][] visited;
-    public static int[] dy = {0,0,1,-1,0,0};
-    public static int fcount = 0;
-    public static int tcount = 0;
-    public static int zcount = 0;
-    public static Queue<Point> q;
+	static void board(int[][][] arr) {
+//		for (int[][] ia : arr) {
+//			for (int[] ib : ia) {
+//				System.out.println(Arrays.toString(ib));
+//			}
+//			System.out.println();
+//		}
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < M; k++) {
+					System.out.print(arr[j][k][i] + " ");
+				}
+				System.out.println();
+			}
+			System.out.println("----------");
+		}
+		System.out.println("=======");
+	}
 
-    public static void tomato2(int x, int y){
-        if (list[x][y] == 0) {
-            list[x][y] = 1;
-            if(visited[x][y]!=1){
-                q.add(new Point(x,y));
-                visited[x][y]= 1;
-            }
-            tcount++;
-        }
-    }
+	static int getAmount(int[][][] arr) {
+		int amnt = 0;
+		for (int[][] ia : arr) {
+			for (int[] ib : ia) {
+				for (int val : ib) {
+					if (val == 0)
+						amnt++;
+				}
+			}
+		}
+		return amnt;
+	}
 
-    public static void tomato(int x, int y,int[] dx){
-        for(int i = 0 ; i <6 ; i++){
-            int nx = x +dx[i];
-            int ny = y + dy[i];
-            int high = x/m;
-            if(i==4&&nx<m*h){
-                tomato2(nx,ny);
-            }
-            else if(i==5&&nx>=0){
-                tomato2(nx,ny);
-            }
-            else if(nx>=m*high&&nx<m*high+m&&ny>=0&&ny<n) {
-                tomato2(nx,ny);
-        }
-        }
+	static void ripe(int m, int n, int h, int[][][] arr) {
+		int ni, nj, nk;
+		for (int i = 0; i < 6; i++) {
+			ni = n + di[i];
+			nj = m + dj[i];
+			nk = h + dk[i];
+			if (0 <= ni && N > ni && 0 <= nj && M > nj && 0 <= nk && H > nk && arr[ni][nj][nk] == 0) {
+				arr[ni][nj][nk] = 1;
+				//board(arr);
+				unripedT--;
+				q.add(new int[] { nj, ni, nk});
+			}
+		}
+	}
 
-    }
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
 
-
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        String [] s = bf.readLine().split(" ");
-        n = Integer.parseInt(s[0]);
-        m = Integer.parseInt(s[1]);
-        h = Integer.parseInt(s[2]);
-
-        list = new int[m*h][n];
-        visited = new int[m*h][n];
-        q = new LinkedList<>();
-        int[] dx = {1,-1,0,0,m,-m};
-
-
-        for(int i = 0 ; i<m*h ; i++){
-            s = bf.readLine().split(" ");
-            for(int j = 0 ; j < n ; j++){
-                int a = Integer.parseInt(s[j]);
-                list[i][j] = a;
-                if (a==1){
-                    visited[i][j] = 1;
-                    q.add(new Point(i,j));
-                    tcount++;
-                    fcount++;
-                }
-                if(a==0)
-                    zcount++;
-            }
-        }
-        int day = 0;
-        while(true){
-            int tmp = tcount;
-            int qSize = q.size();
-            for(int i = 0 ; i< qSize;i++){
-                Point p = q.poll();
-                tomato(p.x,p.y,dx);
-            }
-            day++;
-            if(tmp == tcount){
-                if(day ==1){
-                    int a =0;
-                    for(int i = 0 ; i<m;i++){
-                        for(int j = 0 ; j < n ; j++){
-                            if(list[i][j]==0){
-                                a=1;
-                            }
-                        }
-                    }
-                    if(a==1||tcount==0)
-                        System.out.println(-1);
-                    else
-                        System.out.println(0);
-                    break;
-                }
-                else if(tcount == zcount+fcount){
-                    day--;
-                    System.out.println(day);
-                    break;
-                }
-                else{
-                    System.out.println(-1);
-                    break;
-                }
-            }
-
-        }
-    }
+		int[][][] arr = new int[N][M][H];
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < N; j++) {
+				st = new StringTokenizer(br.readLine());
+				for (int k = 0; k < M; k++) {
+					arr[j][k][i] = Integer.parseInt(st.nextToken());
+				}
+			}
+		}
+		unripedT = getAmount(arr);
+		if (unripedT == 0) {
+			System.out.println(0);
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				for (int k = 0; k < H; k++) {
+					if (arr[i][j][k] == 1) {
+						q.add(new int[] { j, i, k });
+					}
+				}
+			}
+		}
+		int ans = 0;
+		while (!q.isEmpty()) {
+			int n = q.size();
+			for (int i = 0; i < n; i++) {
+				int[] l = q.poll();
+				ripe(l[0], l[1], l[2], arr);
+			}
+			ans++;
+			//System.out.println("day" + ans);
+		}
+		if (getAmount(arr) > 0) {
+			System.out.println(-1);
+			return;
+		}
+		System.out.println(ans-1);
+	}
 }
