dc3c331aba0f083de5cdc8ef34148caeb274fe2c
hen715 <130031561+hen715@users.noreply.github.com>
2025-08-08 14:59:45 +0900

    [D3] Title: 햄버거 다이어트, Time: 707 ms, Memory: 27,648 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,43 +1,57 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution {
+    public static int[][] list;
+    public static int max;
+    public static boolean[] visit;
+    public static int N;
+    public static int L;
     public static void main(String[] args) throws IOException {
-        //System.setIn(new FileInputStream("res/input_d3_5215.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for(int t = 1 ; t <= T ; t++) {
             String[] s  = br.readLine().split(" ");
-            int N = Integer.parseInt(s[0]);
-            int L = Integer.parseInt(s[1]);
-            int[][] list = new int[N + 1][2];
-            for(int i = 1 ; i < N + 1 ; i++){
+            N = Integer.parseInt(s[0]);
+            L = Integer.parseInt(s[1]);
+            list = new int[N][2];
+            for(int i = 0 ; i < N ; i++){
                 s  = br.readLine().split(" ");
                 list[i][0] = Integer.parseInt(s[0]);
                 list[i][1] = Integer.parseInt(s[1]);
             }
-            int[][] dp = new int [N+1][L+1];
-            for(int i = 1 ; i < N + 1 ; i++){
-                for(int j = 1 ; j < L+1 ; j++){
-                    if(j<list[i][1]){
-                        dp[i][j] = dp[i-1][j];
-                    }
-                    else{
-                        dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-list[i][1]] + list[i][0]);
-                    }
-                }
-            }
+            max = Integer.MIN_VALUE;
+            visit = new boolean[N];
+            search(0);
 
-            sb.append('#').append(t).append(' ').append(dp[N][L]);
+            sb.append('#').append(t).append(' ').append(max);
             if(t!=T) {
                 sb.append('\n');
             }
         }
         System.out.print(sb);
-
     }
 
+    public static void search(int depth){
+        if(depth==N){
+            int sum = 0;
+            int kal = 0;
+            for(int i = 0 ; i < N ; i++){
+                if(visit[i]){
+                    sum += list[i][0];
+                    kal += list[i][1];
+                }
+            }
+            if(kal<=L){
+                max = Math.max(max, sum);
+            }
+            return;
+        }
+        visit[depth] = true;
+        search(depth + 1);
+        visit[depth] = false;
+        search(depth + 1);
+    }
 }
