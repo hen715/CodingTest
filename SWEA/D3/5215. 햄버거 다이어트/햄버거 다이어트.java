782476081972ff41f0ec0db30949de7cba7f7deb
hen715 <130031561+hen715@users.noreply.github.com>
2025-08-12 14:56:53 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,020 ms, Memory: 30,220 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,11 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Solution {
     public static int[][] list;
     public static int max;
-    public static boolean[] visit;
+    public static int[] selected;
     public static int N;
     public static int L;
     public static void main(String[] args) throws IOException {
@@ -23,8 +24,37 @@ public class Solution {
                 list[i][1] = Integer.parseInt(s[1]);
             }
             max = Integer.MIN_VALUE;
-            visit = new boolean[N];
-            search(0);
+            for(int i = 1 ; i <= N ; i++){
+                selected = new int[N];
+                for(int j = 0 ; j < i ; j++){
+                    selected[j] =1;
+                }
+                Arrays.sort(selected);
+                int sum = 0;
+                int kal = 0;
+                for(int j = 0 ; j < N ; j++){
+                    if(selected[j]==1) {
+                        sum += list[j][0];
+                        kal += list[j][1];
+                    }
+                }
+                if(kal<=L){
+                    max = Math.max(max, sum);
+                }
+                while (nextPerm()){
+                    sum = 0;
+                    kal = 0;
+                    for(int j = 0 ; j < N ; j++){
+                        if(selected[j]==1) {
+                            sum += list[j][0];
+                            kal += list[j][1];
+                        }
+                    }
+                    if(kal<=L){
+                        max = Math.max(max, sum);
+                    }
+                }
+            }
 
             sb.append('#').append(t).append(' ').append(max);
             if(t!=T) {
@@ -34,24 +64,23 @@ public class Solution {
         System.out.print(sb);
     }
 
-    public static void search(int depth){
-        if(depth==N){
-            int sum = 0;
-            int kal = 0;
-            for(int i = 0 ; i < N ; i++){
-                if(visit[i]){
-                    sum += list[i][0];
-                    kal += list[i][1];
-                }
-            }
-            if(kal<=L){
-                max = Math.max(max, sum);
-            }
-            return;
-        }
-        visit[depth] = true;
-        search(depth + 1);
-        visit[depth] = false;
-        search(depth + 1);
+    public static boolean nextPerm(){
+        int i = N - 2;
+        while (i >= 0 && selected[i] >= selected[i + 1]) i--;
+        if (i < 0) return false;
+        int j = N - 1;
+        while (selected[i] >= selected[j]) j--;
+        swap(i, j);
+        reverse( i + 1, N - 1);
+        return true;
+    }
+    static void swap(int i, int j) {
+        int tmp = selected[i];
+        selected[i] = selected[j];
+        selected[j] = tmp;
+    }
+
+    static void reverse(int s, int e) {
+        while (s < e) swap(s++, e--);
     }
 }
